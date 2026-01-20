f068af5e5c195efb9f2daf2347440d49c3966e3b
Cameron Brown <veiledjune@gmail.com>
2026-01-20 18:47:51 +0200

    refactor: app.js logic into a factory function

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,18 +1,23 @@
-async function getWeatherData(location, unit) {
-  const apiKey = 'QE58399HK3Y6CN2DJ2QAQKKNG';
-  try {
-    const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}}?unitGroup=${unit}&key=${apiKey}
+export const app = (() => {
+  let unitGroup = 'metric';
+  const getWeatherData = async (location, unit) => {
+    const apiKey = 'QE58399HK3Y6CN2DJ2QAQKKNG';
+    try {
+      const response = await fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}}?unitGroup=${unit}&key=${apiKey}
   `,
-    );
-    const weatherData = await response.json();
-    return weatherData;
-  } catch (error) {
-    throw new Error(error);
-  }
-}
+      );
+      const weatherData = await response.json();
+      return weatherData;
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
 
-export async function getWeatherObject(location, unit) {
-  const weatherData = await getWeatherData(location, unit);
-  return weatherData;
-}
+  const getWeatherObject = async (location, unit) => {
+    const weatherData = await getWeatherData(location, unit);
+    return weatherData;
+  };
+
+  return { getWeatherObject };
+})();
