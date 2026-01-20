de1e45efdd2d12098e21c5c55f8f57388de1a08f
Cameron Brown <veiledjune@gmail.com>
2026-01-20 22:45:04 +0200

    feat: Add search helper function and add unit toggle button events
    Add a helper function to handle getting and rendering weather data

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -1,16 +1,38 @@
 import { app } from './app';
 import { render } from './render';
+
 export const events = (() => {
   const weatherFormEvents = () => {
-    const searchInput = document.getElementById('weather-searchbar');
     const searchButton = document.querySelector('.weather-search-btn');
     searchButton.addEventListener('click', async () => {
-      const location = searchInput.value;
-      if (location) {
-        const weatherData = await app.getWeatherObject(location);
-        render(weatherData);
-      }
+      search();
     });
   };
-  return { weatherFormEvents };
+
+  const unitButtonEvents = () => {
+    const toggleUnitButton = document.querySelector('.header-toggle-btn');
+    const celsiusSpan = document.querySelector('.celsius-span');
+    const fahrenheitSpan = document.querySelector('.fahrenheit-span');
+    toggleUnitButton.addEventListener('click', async () => {
+      app.toggleUnitGroup();
+      if (celsiusSpan.classList.contains('--active')) {
+        celsiusSpan.classList.remove('--active');
+        fahrenheitSpan.classList.add('--active');
+      } else {
+        fahrenheitSpan.classList.remove('--active');
+        celsiusSpan.classList.add('--active');
+      }
+      search();
+    });
+  };
+  return { weatherFormEvents, unitButtonEvents };
 })();
+
+async function search() {
+  const searchInput = document.getElementById('weather-searchbar');
+  const location = searchInput.value;
+  if (location) {
+    const weatherData = await app.getWeatherObject(location);
+    render(weatherData);
+  }
+}
